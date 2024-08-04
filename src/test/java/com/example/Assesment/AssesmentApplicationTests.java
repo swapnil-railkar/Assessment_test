b40e37b82937ca7441009adbf6b3778a45ed7caf
Swapnil Railkar <swapnilrailkar01@gmail.com>
2024-08-04 20:44:25 +0530

    Initial Commit

---
+++ src/test/java/com/example/Assesment/AssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Assesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssesmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
